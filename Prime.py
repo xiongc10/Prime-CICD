68a89bc84c6f65cafd6361777eabc9f361250a1c
xiongc10 <xiongc10@uww.edu>
2025-03-05 09:00:42 -0600

    Prime.py

--- Prime.py
+++ Prime.py
@@ -1,20 +1,20 @@
-def is_prime (n):
-if n < 2:
-return False
-for i in range (2, int(n ** 0.5) + 1):
-if n % i == 0:
-return False
-return True
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-def generate_primes (n):
-primes = []
-num = 2
-while len( primes ) < n:
-if is_prime (num):
-primes . append (num)
-num += 1
-return primes
+def generate_primes(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
 
-if __name__ == " __main__ ":
-n = int( input ("Enter the number of primes to generate : "))
-print (f" First {n} prime numbers : { generate_primes (n)}")
+if __name__ == "__main__":
+    n = int(input("Enter the number of primes to generate: "))
+    print(f"First {n} prime numbers: {generate_primes(n)}")
